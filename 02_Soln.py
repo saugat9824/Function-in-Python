0f4964733d37343ad62c6770a622d475f056fedd
Saugat <saugatrijal99@gmail.com>
2024-05-29 09:05:14 +0545

    Solution 2 and 3

---
+++ 02_Soln.py
@@ -0,0 +1,9 @@
+# Function with multiple Parameters.
+# Create a function that takes two number as parameters and return their sum.
+
+
+def add(numOne, numTwo):
+    return numOne + numTwo
+
+
+print(add(5, 5))
---
+++ 03_Soln.py
@@ -0,0 +1,10 @@
+# Polymorphism in Function
+# Write a function multiply that multiplies two numbers, but can also accept and multiply strings.
+
+def multiply(p1, p2):
+    return p1 * p2
+
+
+print(multiply(5, 10))
+print(multiply('a', 6))
+print(multiply(5, 'G'))
