6753d5d7ef34472f5fb5462cf1cdd285b7a76da1
Saugat <saugatrijal99@gmail.com>
2024-05-29 08:48:53 +0545

    Solution 1

---
+++ 01_Soln.py
@@ -0,0 +1,10 @@
+# Basic function syntax
+# Write a function to calculate and return the square of a number.
+
+def square(number): # create a function square, give parameter number. number is variable 
+    #print(number ** 2) 
+    return number ** 2
+
+# square(4)
+result = square(5) # to store square in result.
+print("Square is: ", result)
